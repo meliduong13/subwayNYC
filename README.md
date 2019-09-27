35678b3c23df6dae9859f88c9484e7775f685279
meliduong13 <meliduong13@gmail.com>
2019-09-27 11:43:40 -0400

    Correction to code with feedback: - Added information regarding the installation of packages - Removed a method that was not used and remove code lines that weren't needed

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # subwayNYC
 This is a small console application that displays all stops and their GPS coordinates, for a chosen New York Subway line. 
 
+###Install packages
+- Before running the program, run the following command:
+  - ```pip install requests```
 ### How to run the console app
 - In the current directory ```subwayNYC```, run the following command:
   - ```python Subway.py```
--- Subway.py
+++ Subway.py
@@ -15,22 +15,6 @@ with open('stops.txt', mode='r') as csv_file:
         stops_dict[row["stop_id"]] = Stop(row["stop_name"], row["stop_lat"], row["stop_lon"],
                                           row["location_type"], row["parent_station"])
 
-
-# method to handle server exception. For example, an exception could be thrown when the server is down
-def access_url(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except HTTPError:
-        print(f'An error with the server occurred, try again later.')
-        sys.exit(f'Program exiting')
-    except Exception:
-        print(f'An error occurred, try again later.')
-        sys.exit(f'Program exiting')
-    else:
-        return response
-
-
 url_get_all_subway_lines = 'http://traintimelb-367443097.us-east-1.elb.amazonaws.com/getSubwaylines'
 
 # retrieve all subway lines
@@ -78,9 +62,12 @@ while loop:
         for station in borough["stations"]:
             # printing the station name, id, longitude and latitude. The two last attributes are found in the
             # stops_dict dictionary. We get the information of a Stop object by retrieving it by it's id
-            print('{0} ({1}): {2}, {3}'
-                  .format(station["name"], station["id"], stops_dict[station["id"]].get_longitude(),
-                          stops_dict[station["id"]].get_latitude()))
+            if station["id"] not in stops_dict.keys():
+                print('No information exists for stop {0} with id {1}'.format(station["name"], station["id"]))
+            else:
+                print('{0} ({1}): {2}, {3}'
+                      .format(station["name"], station["id"], stops_dict[station["id"]].get_longitude(),
+                              stops_dict[station["id"]].get_latitude()))
     print('\n')
 
     # allow the user to stop the program or continue querying another line
@@ -91,9 +78,3 @@ while loop:
     else:
         print('Thank you for using this application!')
         loop = False
-
-#  below I am reassinging these values once the program ends. This is to be able to test the exception catching method
-#  'handle_url_response' from the class 'Helpers'
-lines_list = None
-stops_dict = None
-json_stops = None
